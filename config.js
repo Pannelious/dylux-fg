177af811a7c4184ad65cffa556695ab2fd58ec7d
Pannelious <101747535+Pannelious@users.noreply.github.com>
2023-07-02 22:54:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['263788914423', 'FG98', true],
+  ['263788914422'], 
+  ['true'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['26388914422'] 
+global.prems = ['263788914422', '263788914422', '263788914422']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
@@ -25,10 +25,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'DyLux┃ᴮᴼᵀ' 
-global.author = '@fg98' 
+global.packname = 'MASTERPEACE ┃ᴮᴼᵀ' 
+global.author = '@Masterpeace ' 
 global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff\n' 
-global.dygp = 'https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU'
+global.dygp = 'https://chat.whatsapp.com/KAwMHb2skPB0i5PUTyAvmH'
 global.fgsc = 'https://github.com/FG98F/dylux-fg' 
 global.fgyt = 'https://youtube.com/fg98f'
 global.fgpyp = 'https://paypal.me/fg98f'
